347a698f77d3ad3cd35afbd87aadcbf306b52923
Big Tony <61833383+MisteriumEz@users.noreply.github.com>
2021-07-21 15:54:14 -0300

    testes
    testando os testes testosos testudos

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+importScripts("/precache-manifest.1d438fcb67677f097add58603a023461.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
