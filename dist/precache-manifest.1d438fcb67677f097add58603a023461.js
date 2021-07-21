6e071292779b72ae60a9cd83cd4a6a9195e75d79
Big Tony <61833383+MisteriumEz@users.noreply.github.com>
2021-07-21 15:48:42 -0300

    Testes
    apenas testando, tem nada demais

---
+++ dist/precache-manifest.1d438fcb67677f097add58603a023461.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7645c1a29bf47145b323",
+    "url": "/main.css"
+  },
+  {
+    "revision": "7645c1a29bf47145b323",
+    "url": "/main.bundle.js"
+  },
+  {
+    "revision": "17420d012d1e45670160",
+    "url": "/1.css"
+  },
+  {
+    "revision": "17420d012d1e45670160",
+    "url": "/1.main.bundle.js"
+  },
+  {
+    "revision": "9bf71cffdcbbb424e73a",
+    "url": "/2.main.bundle.js"
+  },
+  {
+    "revision": "36e6a06a4146af2f966ffcf021751724",
+    "url": "/fonts/GeosansLight.ttf"
+  },
+  {
+    "revision": "58d510f5ce61f06ce54e75b890b3b62d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa230c624c7a16cbeeecf040bdd2b72f",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "9a59769555a9217138e347bbf9b51a3d",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "d5b4b6b9e39f8294a57ab14a8e7def4e",
+    "url": "/icons/icon_512x512.png"
+  },
+  {
+    "revision": "2c1fadb85e04b499297e02547ece89de",
+    "url": "/icons/icon_196x196.png"
+  },
+  {
+    "revision": "b2475651265c9fb1cb1424308b95ebc5",
+    "url": "/icons/icon_256x256.png"
+  }
+];
